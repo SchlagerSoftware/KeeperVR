864cd78a2dcf6a6e241e5e5eee63995cfbd3beae
Kozik Przemysław <Przemyslaw.Kozik@assecods.pl>
2022-10-10 21:00:47 +0200

    Ball controller with shot functionality

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject targetPlane;
+
+    [SerializeField]
+    private float speed = 0.001f;
+
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        this.rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //! MOCK UNITY NO START GAME BUTTON
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.Shot();
+        }
+    }
+
+    public void Shot()
+    {
+        Vector3 target = this.GenerateTargetPoint();
+        this.rb.AddForce((target - this.transform.position).normalized * this.speed + new Vector3(0, 3f, 0), ForceMode.Impulse);
+    }
+
+    private Vector3 GenerateTargetPoint()
+    {
+        Bounds bounds = this.targetPlane.GetComponent<Collider>().bounds;
+        print(bounds.min);
+        print(bounds.max);
+        return new Vector3(
+        Random.Range(bounds.min.x, bounds.max.x),
+        Random.Range(bounds.min.y, bounds.max.y),
+        Random.Range(bounds.min.z, bounds.max.z)
+    );
     }
 }
