712001448f894bda1425c05f4ef94ee785d20f08
Oliwer Szefer <oszefer@man.poznan.pl>
2022-10-20 23:05:32 +0200

    Added a pause menu

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverMenu : MonoBehaviour
+public class PauseMenuController : MonoBehaviour
 {
 
     public void Restart()
